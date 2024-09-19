cecc2d205de6779056ce1689cd1fe5f28c321614
Naglaa99 <Naglaahamdi6@gmail.com>
2024-09-19 04:02:37 +0200

    create reveal-on-scroll animations for elements

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,3 @@
-window.onscroll = () => stickyHeader();
-function stickyHeader() {
-  const header = document.getElementById("header");
-  if (window.innerWidth > 768) {
-    if (window.scrollY > 0) header.classList.add("sticky");
-    else header.classList.remove("sticky");
-  } else header.classList.remove("sticky");
-}
 const toggleMenu = document.getElementById("toggle-menu");
 const navLinks = document.querySelector(".nav-links");
 const navLink = document.querySelectorAll(".nav-links li a");
@@ -20,3 +12,54 @@ navLink.forEach((link) => {
     icon.classList.remove("bx-x");
   });
 });
+const scrollRevealOption= {
+  origin:'bottom',
+  distance: '50px',
+  duration: 1000,
+  easing: 'ease-in-out',
+};
+ScrollReveal().reveal('.location-hero h1', scrollRevealOption);
+ScrollReveal().reveal('.form-search', {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal('.location-hero img', {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".range-card", {
+  duration: 1000,
+  interval: 500,
+});
+ScrollReveal().reveal(".location-image img",{
+  ...scrollRevealOption,
+  origin:"right",
+});
+ScrollReveal().reveal(".location-details h2",{
+  ...scrollRevealOption,
+  delay:500,
+});
+ScrollReveal().reveal(".location-details p",{
+  ...scrollRevealOption,
+  delay:1000,
+});
+ScrollReveal().reveal(".location-details button",{
+  ...scrollRevealOption,
+  delay:1500,
+})
+ScrollReveal().reveal(".story-card",{
+  ...scrollRevealOption,
+  interval:500,
+})
+ScrollReveal().reveal(".download-image img",{
+  ...scrollRevealOption,
+  origin:"right",
+})
+ScrollReveal().reveal(".download-details h2",{
+  ...scrollRevealOption,
+ delay:500,
+})
+ScrollReveal().reveal(".download-link",{
+  ...scrollRevealOption,
+  delay:1000,
+})
